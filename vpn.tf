774bae4cb0b489125f4138e4a2968817e0c217bd
Guillaume Khayat <guillaume.kh.alt@gmail.com>
2023-03-31 00:20:02 +0200

    upgrade ubuntu

--- vpn.tf
+++ vpn.tf
@@ -9,16 +9,16 @@ provider "aws" {
   region     = var.aws-region
 }
 
-# Find latest Ubuntu 20.04 LTS
-data "aws_ami" "latest-ubuntu-focal-fossa-lts-ami" {
-  owners  = ["099720109477"]
+# Find latest Ubuntu 22.04 AMI
+data "aws_ami" "ubuntu-ami" {
+  owners  = ["099720109477"] # Official Ubuntu AMIs
   filter {
     name   = "state"
     values = ["available"]
   }
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-arm64*"]
   }
   most_recent = true
 }
@@ -82,7 +82,7 @@ data "template_cloudinit_config" "user_data" {
 
 # Create EC2 instance
 resource "aws_instance" "aws-instance" {
-  ami           = data.aws_ami.latest-ubuntu-focal-fossa-lts-ami.image_id
+  ami           = data.aws_ami.ubuntu-ami.image_id
   instance_type = "t4g.nano"
   vpc_security_group_ids  = [ aws_security_group.security-group.id ]
   ipv6_address_count  = 1
